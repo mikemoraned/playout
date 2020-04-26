6aaca3e9b06fd4a591b09744d094f9f3403377db
Mike Moran <mike@houseofmoran.com>
2020-04-26 23:54:08 +0100

    add top Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from "react";
 import "./App.scss";
 
+function Navigation() {
+  return (
+    <nav
+      className="navbar is-light"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://playout.houseofmoran.io/">
+          <span
+            style={{
+              "margin-left": "0.5em",
+              font: "small-caps bold 25px monospace",
+            }}
+          >
+            P L A Y O U T
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <section className="section">
         <div className="container">
           <h1 className="title">Hello World</h1>

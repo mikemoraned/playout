4c8280e965e9a9542b151e075265d1e2310e1585
Mike Moran <mike@houseofmoran.com>
2020-05-31 23:43:02 +0100

    0.3.1: add more compact v2 format for grid
    - both v1 and v2 will be supported, but
      v2 selected by default as it is more
      compact and therefore less likely
      to hit URL size limits when we start
      adding serialisation of team and bias
      specs later
    - format is based on base64 encoding of
      seats encoded as a binary string e.g.
      ```
      .~.~
      -> 0101 binary
      -> 01010000 padded binary
      -> 80 int char code
      -> "P" string
      -> "UA==" base64
      -> "UA--" url-safe
      ```

--- src/model/problem.js
+++ src/model/problem.js
@@ -11,22 +11,45 @@ export const GridSpec = types
     height: types.integer,
     seats: types.array(types.string),
   })
-  .views((self) => ({
-    toVersion1Format() {
-      let spaceString = "";
+  .views((self) => {
+    function toSeatString(missingChar, presentChar) {
+      let seatString = "";
       for (let y = 0; y < self.height; y++) {
         for (let x = 0; x < self.width; x++) {
           const position = positionFor(x, y);
           if (self.seats.indexOf(position) === -1) {
-            spaceString += ".";
+            seatString += missingChar;
           } else {
-            spaceString += "~";
+            seatString += presentChar;
           }
         }
       }
-      return `${self.width}x${self.height}${spaceString}_v1`;
-    },
-  }));
+      return seatString;
+    }
+
+    return {
+      toVersion1Format() {
+        return `${self.width}x${self.height}${toSeatString(".", "~")}_v1`;
+      },
+      toVersion2Format() {
+        let binarySeatString = toSeatString("0", "1");
+        if (binarySeatString.length % 8 !== 0) {
+          const numNeeded = 8 - (binarySeatString.length % 8);
+          binarySeatString += "0".repeat(numNeeded);
+        }
+        let charCodes = [];
+        for (let offset = 0; offset < binarySeatString.length; offset += 8) {
+          charCodes.push(
+            parseInt(binarySeatString.slice(offset, offset + 8), 2)
+          );
+        }
+        const bytesString = String.fromCharCode(...charCodes);
+        const base64 = btoa(bytesString);
+        const urlSafe = mapBase64ToUrlSafeChars(base64);
+        return `${self.width}x${self.height}_${urlSafe}v2`;
+      },
+    };
+  });
 
 export const Problem = types
   .model("Problem", {
@@ -51,6 +74,26 @@ export const Problem = types
 
 export class InvalidProblemSpec extends Error {}
 
+function mapBase64ToUrlSafeChars(base64) {
+  return remap("+/=", "._-", base64);
+}
+
+function mapUrlSafeCharsToBase64(urlSafe) {
+  return remap("._-", "+/=", urlSafe);
+}
+
+function remap(from, to, s) {
+  if (from.length !== to.length) {
+    throw new Error(`${from} and ${to} must have same length`);
+  }
+  const map = new Map();
+  for (let index = 0; index < from.length; index++) {
+    map.set(from.charAt(index), to.charAt(index));
+  }
+  const regex = new RegExp(`[${from}]`, "g");
+  return s.replace(regex, (f) => (map.has(f) ? map.get(f) : f));
+}
+
 export function parseProblemFrom(gridSpec) {
   if (gridSpec === null || gridSpec === undefined) {
     throw new InvalidProblemSpec("missing spec");
@@ -60,11 +103,21 @@ export function parseProblemFrom(gridSpec) {
 }
 
 function parseGridSpec(gridSpec) {
-  const regex = /(\d+)x(\d+)([.~]+)_v1/;
-  const match = gridSpec.match(regex);
-  if (match === null) {
+  const v1Regex = /(\d+)x(\d+)([.~]+)_v1/;
+  const v1Match = gridSpec.match(v1Regex);
+  const v2Regex = /(\d+)x(\d+)_([0-9A-Za-z._]+-{0,2})v2/;
+  const v2Match = gridSpec.match(v2Regex);
+  if (v1Match === null && v2Match === null) {
     throw new InvalidProblemSpec("invalid spec: incorrect format");
   }
+  if (v1Match !== null) {
+    return parseV1GridSpec(v1Match);
+  } else {
+    return parseV2GridSpec(v2Match);
+  }
+}
+
+function parseGridDimensions(match) {
   const width = parseInt(match[1]);
   const height = parseInt(match[2]);
   if (width === 0 || height === 0) {
@@ -76,7 +129,20 @@ function parseGridSpec(gridSpec) {
       `invalid spec: width * height too large, must be <= ${maxArea}`
     );
   }
-  const seatString = match[3];
+  return { width, height };
+}
+
+function parseV1GridSpec(match) {
+  const { width, height } = parseGridDimensions(match);
+  const seats = parseSeatsSpec(width, height, match[3]);
+  return GridSpec.create({
+    width,
+    height,
+    seats,
+  });
+}
+
+function parseSeatsSpec(width, height, seatString) {
   const expectedSpaceCharacters = width * height;
   if (seatString.length !== expectedSpaceCharacters) {
     throw new InvalidProblemSpec(
@@ -92,11 +158,40 @@ function parseGridSpec(gridSpec) {
       }
     }
   }
+  return seats;
+}
+
+function parseV2GridSpec(match) {
+  const { width, height } = parseGridDimensions(match);
+  const urlSafeBase64String = match[3];
+  const base64String = mapUrlSafeCharsToBase64(urlSafeBase64String);
+  const bytesString = parseBase64(base64String);
+  let binarySeatString = "";
+  for (let index = 0; index < bytesString.length; index++) {
+    binarySeatString += bytesString
+      .charCodeAt(index)
+      .toString(2)
+      .padStart(8, "0");
+  }
+  const seatString = remap("01", ".~", binarySeatString).slice(
+    0,
+    width * height
+  );
+  const seats = parseSeatsSpec(width, height, seatString);
+
   return GridSpec.create({
     width,
     height,
     seats,
   });
+}
+
+function parseBase64(base64) {
+  try {
+    return atob(base64);
+  } catch (e) {
+    throw new InvalidProblemSpec(`invalid spec: cannot decode from base64`);
+  }
 }
 
 function defaultTeams() {
@@ -120,7 +215,7 @@ export function randomHardGridSpec() {
   return randomGridSpec(5, 5);
 }
 
-function randomGridSpec(width, height) {
+export function randomGridSpec(width, height) {
   const minimumSeats = defaultTeams().totalMembers;
   const seats = [];
   while (seats.length < minimumSeats) {

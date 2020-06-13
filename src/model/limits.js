c3dd69289f84ce82dabd89d2cc7a3ebcb1e764af
Mike Moran <mike@houseofmoran.com>
2020-06-13 17:52:23 +0100

    limit max area to 100

--- src/model/limits.js
+++ src/model/limits.js
@@ -1,7 +1,7 @@
 import { InvalidProblemSpec } from "./invalid_problem_spec";
 
 export function validateArea(width, height) {
-  const maxArea = 10000;
+  const maxArea = 100;
   if (width * height > maxArea) {
     throw new InvalidProblemSpec(
       `invalid spec: width * height too large, must be <= ${maxArea}`

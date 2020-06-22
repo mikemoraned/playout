1f5698e767c9729b4f7166ba55df218859095361
Mike Moran <mike@houseofmoran.com>
2020-06-22 22:32:05 +0100

    add NextProblem button which changes problem

--- src/components/Evaluation.js
+++ src/components/Evaluation.js
@@ -1,9 +1,50 @@
 import React from "react";
 import { useContext } from "react";
 import { observer } from "mobx-react";
+// import { gql } from "apollo-boost";
+// import { useQuery } from "@apollo/react-hooks";
+import { useHistory } from "react-router-dom";
 import { StoreContext } from "../model/contexts.js";
 import { Progress } from "./Progress";
 import { Scoring } from "./Scoring";
+import { randomEasyProblem } from "../model/problem.js";
+
+export const NextProblem = observer(() => {
+  const history = useHistory();
+  const { store } = useContext(StoreContext);
+  const { progress } = store.evaluation;
+  const complete = progress.value === progress.max;
+
+  function visitRandomProblem() {
+    const problem = randomEasyProblem();
+    const path = `/play/${problem.grid.toVersion2Format()}/${problem.teams.toVersion1Format()}`;
+    history.push(path);
+  }
+
+  return (
+    <>
+      <button
+        className="button is-hidden-mobile"
+        disabled={!complete}
+        onClick={() => visitRandomProblem()}
+      >
+        <span>Next</span>
+        <span className="icon">
+          <i className="fas fa-angle-double-right"></i>
+        </span>
+      </button>
+      <button
+        className="button is-hidden-tablet"
+        disabled={!complete}
+        onClick={() => visitRandomProblem()}
+      >
+        <span className="icon">
+          <i className="fas fa-angle-double-right fa-2x"></i>
+        </span>
+      </button>
+    </>
+  );
+});
 
 export const Evaluation = observer(() => {
   const { store } = useContext(StoreContext);
@@ -16,7 +57,7 @@ export const Evaluation = observer(() => {
   return (
     <div className="box">
       <div className="columns is-mobile is-vcentered">
-        <div className="column is-2 has-text-right">
+        <div className="column is-1 has-text-right">
           <span
             style={{
               fontFamily: "monospace",
@@ -28,8 +69,11 @@ export const Evaluation = observer(() => {
         <div className="column is-6">
           <Progress />
         </div>
-        <div className="column is-4 has-text-centered">
+        <div className="column is-2 has-text-centered">
           <Scoring />
+        </div>
+        <div className="column is-3 has-text-centered">
+          <NextProblem />
         </div>
       </div>
     </div>

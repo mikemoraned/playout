5686215eb0fd5c583a1536e7310f0a43808166b8
Mike Moran <mike@houseofmoran.com>
2020-07-18 15:35:40 +0100

    factor out Tutorial contexts and components

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,140 +1,17 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 import { Grid } from "../components/Grid";
 import { TeamsMini } from "../components/TeamsMini";
 import { TeamsFull } from "../components/TeamsFull";
 import { Evaluation } from "../components/Evaluation";
-import { StoreProvider } from "../model/contexts.js";
+import { StoreProvider, TutorialProvider } from "../model/contexts.js";
 import { Biases } from "../components/Biases";
+import { TutorialButton, TutorialSetup } from "../components/Tutorial";
 import { useParams, Redirect } from "react-router-dom";
 import { parseProblemFrom } from "../model/problem";
 import { observer } from "mobx-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBorderAll,
-  faUser,
-  faStar,
-  faQuestionCircle,
-  faArrowCircleRight,
-  faWindowClose,
-} from "@fortawesome/free-solid-svg-icons";
-import { faStopCircle } from "@fortawesome/free-regular-svg-icons";
-import Joyride, { STATUS } from "react-joyride";
+import { faBorderAll, faUser, faStar } from "@fortawesome/free-solid-svg-icons";
 import "./Play.scss";
-import colors from "./Play.colors.scss";
-
-export const TutorialContext = React.createContext(null);
-
-export const TutorialProvider = ({ children }) => {
-  const [showTutorial, setShowTutorial] = useState(false);
-  return (
-    <TutorialContext.Provider value={[showTutorial, setShowTutorial]}>
-      {children}
-    </TutorialContext.Provider>
-  );
-};
-
-const TutorialTooltip = ({ step, tooltipProps, skipProps, primaryProps }) => {
-  return (
-    <div {...tooltipProps}>
-      <article className="message is-info">
-        <div className="message-header">
-          <p>Step</p>
-
-          <button className="button is-info" aria-label="delete" {...skipProps}>
-            <span className="icon">
-              <FontAwesomeIcon icon={faWindowClose} size="2x" />
-            </span>
-          </button>
-        </div>
-        <div className="message-body">
-          <div className="content">{step.content}</div>
-          <div>
-            <div className="buttons is-right">
-              <button className="button is-primary" {...primaryProps}>
-                <span className="icon">
-                  <FontAwesomeIcon icon={faArrowCircleRight} />
-                </span>{" "}
-                <span>Next</span>
-              </button>
-              <button className="button" {...skipProps}>
-                <span className="icon">
-                  <FontAwesomeIcon icon={faStopCircle} />
-                </span>{" "}
-                <span>Stop</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </article>
-    </div>
-  );
-};
-
-const TutorialButton = () => {
-  const [showTutorial, setShowTutorial] = useContext(TutorialContext);
-  return (
-    <button
-      className="button is-small is-info"
-      onClick={() => setShowTutorial(true)}
-      disabled={showTutorial}
-    >
-      <span className="icon">
-        <FontAwesomeIcon icon={faQuestionCircle} />
-      </span>{" "}
-      <span>Tutorial</span>
-    </button>
-  );
-};
-
-const TutorialSetup = () => {
-  const [showTutorial, setShowTutorial] = useContext(TutorialContext);
-  const steps = [
-    {
-      target: ".grid .seat",
-      content: "Place a Team Member by touching the Seat ...",
-    },
-    {
-      target: ".evaluation .scoring",
-      content: "... which increases your Score ...",
-    },
-    {
-      target: ".tut-biases-explanation",
-      content: "... by understanding team Biases",
-    },
-
-    {
-      target: ".evaluation .completed",
-      content: "Place all of your Team Members ...",
-    },
-    {
-      target: ".evaluation .next",
-      content: "... and move on to the next Game",
-    },
-  ];
-
-  function handleJoyrideStatus(data) {
-    const { status } = data;
-    if ([STATUS.FINISHED, STATUS.SKIPPED].includes(status)) {
-      setShowTutorial(false);
-    }
-  }
-
-  return (
-    <Joyride
-      steps={steps}
-      run={showTutorial}
-      spotlightClicks={true}
-      continuous={true}
-      callback={handleJoyrideStatus}
-      tooltipComponent={TutorialTooltip}
-      styles={{
-        options: {
-          arrowColor: colors.infocolor,
-        },
-      }}
-    />
-  );
-};
 
 const Instance = observer(() => {
   return (
